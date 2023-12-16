c43716b0c474a29165c59c19c082425b9f588b41
toby <tobiby07@gmail.com>
2023-12-16 21:32:57 +0700

    menambahkan fungsi auto generate nim v-1

--- models/tambahMahasiswa.js
+++ models/tambahMahasiswa.js
@@ -1,40 +1,58 @@
 const { knex } = require("../dbConnecting");
 
 const postTambahMahasiswa = async (req, res) => {
-    try {
-      const photopath = req.file ? `/uploads/${req.file.filename}` : null;
-  
-      await knex.table("mahasiswa").insert({
-        nama: req.body.nama,
-        tempatlahir: req.body.tempatlahir,
-        tanggallahir: req.body.tanggallahir,
-        jeniskelamin: req.body.jeniskelamin,
-        alamat: req.body.alamat,
-        alamatdomisili: req.body.alamatdomisili,
-        kontakhp: req.body.kontakhp,
-        email: req.body.email,
-        namaayah: req.body.namaayah,
-        namaibu: req.body.namaibu,
-        namawali: req.body.namawali,
-        kontakhporangtua: req.body.kontakhporangtua,
-        pekerjaanorangtua: req.body.pekerjaanorangtua,
-        asalsekolah: req.body.asalsekolah,
-        tahunlulus: req.body.tahunlulus,
-        jurusan: req.body.jurusan,
-        fakultas: req.body.fakultas,
-        kelas: req.body.kelas,
-        nim: req.body.nim,
-        photo: photopath,
-      });
-  
-      res.redirect("/tambah-mahasiswa");
-    } catch (err) {
-      console.log(err);
-      res.send("An error occurred while adding data.");
-    }
-  };
+  try {
+    const photopath = req.file ? `/uploads/${req.file.filename}` : null;
+    const angkatan = req.body.tahunangkatan;
+    const nextNimValueResult = await knex.raw("SELECT nextval('nim_sequence')");
+    const nextNimValue = '0'+nextNimValueResult.rows[0].nextval ;
+    let kodekelas = "";
 
-  module.exports = {
-    postTambahMahasiswa
+    if (req.body.kelas === "S1-INFORMATIKA") {
+      kodekelas = `${angkatan}983${nextNimValue}`;
+    } else if (req.body.kelas === "S1-ILMU KOMPUTER") {
+      kodekelas = `${angkatan}456${nextNimValue}`;
+    } else if (req.body.kelas === "S1-AKUNTANSI") {
+      kodekelas = `${angkatan}123${nextNimValue}`;
+    } else {
+      kodekelas = `${angkatan}000${nextNimValue}`;
+    }
+
+   
+   
+
+    // Insert the data into the mahasiswa table
+    await knex.table("mahasiswa").insert({
+      nama: req.body.nama,
+      tempatlahir: req.body.tempatlahir,
+      tanggallahir: req.body.tanggallahir,
+      jeniskelamin: req.body.jeniskelamin,
+      alamat: req.body.alamat,
+      alamatdomisili: req.body.alamatdomisili,
+      kontakhp: req.body.kontakhp,
+      email: req.body.email,
+      namaayah: req.body.namaayah,
+      namaibu: req.body.namaibu,
+      namawali: req.body.namawali,
+      kontakhporangtua: req.body.kontakhporangtua,
+      pekerjaanorangtua: req.body.pekerjaanorangtua,
+      asalsekolah: req.body.asalsekolah,
+      tahunlulus: req.body.tahunlulus,
+      jurusan: req.body.jurusan,
+      fakultas: req.body.fakultas,
+      kelas: req.body.kelas,
+      nim: kodekelas,
+      photo: photopath,
+      tahunangkatan: req.body.tahunangkatan,
+    });
+
+    res.redirect("/tambah-mahasiswa");
+  } catch (err) {
+    console.log(err);
+    res.send("An error occurred while adding data.");
   }
-  knex
+};
+
+module.exports = {
+  postTambahMahasiswa,
+};
